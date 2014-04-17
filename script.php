5e9b6b48f8840d6561903bc254a168037de4b279
git <jonhemstreet@gmail.com>
2014-04-17 11:27:52 -0600

    Script setup / Init lastfm integration

---
+++ script.php
@@ -0,0 +1,35 @@
+<?php
+
+// Include Simple html dom
+include_once('./simple_html_dom.php');
+
+$script = array_shift($argv);
+
+echo PHP_EOL . 'Last.fm Report' . PHP_EOL . PHP_EOL;
+
+if(!$argv) {
+    echo "You must pass LastFm url username parameters: i.e jonhemstreet";
+}
+
+// Create DOM from URL or file
+forEach($argv as $userName) {
+
+    $html = file_get_html('http://www.last.fm/user/' . $userName);
+
+    echo $userName . ' --------------------------------------------------' . PHP_EOL . PHP_EOL;
+
+    $x = false;
+
+    forEach ($html->find('#recentTracks tr.first .subjectCell a') as $content) {
+
+        echo($content->innertext);
+        echo ($x) ? '' : ' - ';
+
+        if (!$x) {
+            $x = true;
+        }
+
+    }
+
+    echo PHP_EOL . PHP_EOL . '-------------------------------' . PHP_EOL . PHP_EOL;
+}
